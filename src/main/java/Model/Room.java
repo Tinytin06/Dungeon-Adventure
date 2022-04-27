570d875120ee9a9e5c2d5fdaade918cdbb34a42e
Tinytin06 <52684983+Tinytin06@users.noreply.github.com>
2022-04-27 15:35:24 -0700

    more room stuff

--- src/main/java/Model/Room.java
+++ src/main/java/Model/Room.java
@@ -22,7 +22,18 @@ public class Room {
         if (Math.random() < .1) {
             setMyRoomInventory(FIGHT);
         }
+        if(myRoomInventory.isEmpty()){
+            setMyRoomInventory(NORMAL);
+        }
 
+    }
+
+    protected void setMyDisplayIcon(final String theIcon) {
+        if (theIcon.length() > 2 || theIcon.length() <= 0) {
+            throw new IllegalArgumentException("The Icon string length cannot be greater than 1 or less than 0");//need to change this to not throw exception
+        } else {
+            myDisplayIcon = theIcon;
+        }
     }
 
     void setMyRoomInventory(RoomType theType) {
@@ -48,6 +59,16 @@ public class Room {
         myRoomInventory.clear();
     }
 
+    protected void exploreTheRoom(){
+        if (myRoomInventory.size() == 1) {
+            for (RoomType roomTypes : RoomType.values()) {
+                if (myRoomInventory.contains(roomTypes.type)) {
+                    setMyDisplayIcon(roomTypes + " ");
+                }
+            }
+        }
+    }
+
     public static void main(String[] args) {
     Room myRoom = new Room();
         System.out.println(myRoom.getMyRoomInventory());
@@ -60,6 +81,7 @@ public class Room {
         System.out.println(myRoom.getMyRoomInventory());
         myRoom.removeMyTypes(NORMAL);
         System.out.println(myRoom);
+        myRoom.exploreTheRoom();
     }
 
 }
