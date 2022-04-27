5e3ea0d14579bffd96ffff1c81fa2185cc204a2a
Tinytin06 <52684983+Tinytin06@users.noreply.github.com>
2022-04-27 14:15:44 -0700

    dungeon and room type changes

--- src/main/java/Model/Dungeon.java
+++ src/main/java/Model/Dungeon.java
@@ -7,16 +7,24 @@ import java.util.ArrayList;
 public class Dungeon {
     private int myDungeonSize;
     private ArrayList<ArrayList<Room>> myDungeon;
-    private
 
     Dungeon(final int theDungeonLength) {
         myDungeonSize = theDungeonLength;
         myDungeon = new ArrayList<>();
+        dungeonBuilder();
     }
 
-    private void dungeonBuilder(){
-
+    void dungeonBuilder(){
+        for (int i = 0; i < myDungeonSize; i++) {
+            ArrayList<Room> row = new ArrayList<>();
+            for (int j = 0; j < myDungeonSize; j++) {
+                Room myNewRoom = new Room();
+                row.add(myNewRoom);
+            }
+            myDungeon.add(row);
+        }
     }
+
     public String toString(){
     return "";
     }
--- src/main/java/Model/RoomType.java
+++ src/main/java/Model/RoomType.java
@@ -1,11 +1,17 @@
 package Model;
 
 public enum RoomType {
-     FIGHT('F'), PIT('P'), NORMAL('N'), PILLAR('P');
+    FIGHT('F'),
+    PIT('P'),
+    NORMAL('N'),
+    PILLAR('P'),
+    EXIT('X'),
+    ENTRANCE('E');
 
     public final char type;
+
     RoomType(char theType) {
-    type = theType;
+        type = theType;
     }
 
     public char getType() {
