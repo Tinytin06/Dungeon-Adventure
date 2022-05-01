5ac5c7429529392c75f0fd1dc99866e0a5b9049f
Tinytin06 <52684983+Tinytin06@users.noreply.github.com>
2022-05-01 01:31:44 -0700

    implemented rest of dungeon class and commented

--- src/main/java/Model/Dungeon.java
+++ src/main/java/Model/Dungeon.java
@@ -2,13 +2,27 @@ package Model;
 
 import Model.Characters.Hero;
 
+import java.awt.*;
 import java.util.ArrayList;
 import java.util.Random;
 
+/**
+ * This class manages the dungeon, it is responsible for creating and populating the dungeon
+ * along other operation that are exclusive to the dungeon.
+ * @author Varun Parbhakar
+ * @editor Austin Luu
+ */
 public class Dungeon {
     private int myDungeonSize;
     private ArrayList<ArrayList<Room>> myDungeon;
+    private boolean myCheatEnabled;
 
+    /**
+     * This constructor creates the dungeon with the use of the specified dungeon size from
+     * the user.
+     * @param theDungeonLength
+     *
+     */
     Dungeon(final int theDungeonLength) {
         myDungeonSize = theDungeonLength;
         myDungeon = new ArrayList<>();
@@ -16,6 +30,10 @@ public class Dungeon {
         entranceCreator(myDungeon);
         createExitEntrance(myDungeon, myDungeonSize);
     }
+
+    /**
+     * This method builds a dungeon by creating rooms in the ArrayList
+     */
 
     void dungeonBuilder(){
         for (int i = 0; i < myDungeonSize; i++) {
@@ -28,6 +46,11 @@ public class Dungeon {
             myDungeon.add(row);
         }
     }
+
+    /**
+     * To string method for the dungeon.
+     * @return (String containing the layout of the dungeon)
+     */
     public String toString() {
 
         String dungeonPrint = "\n";
@@ -60,6 +83,12 @@ public class Dungeon {
 
     }
 
+    /**
+     * This method creates and exit, entrance and populates the dungeon with 2 crowns.
+     * @param theDungeon
+     * @param theDungeonSize
+     * need to breakup method cus does too many things
+     */
     private void createExitEntrance(final ArrayList<ArrayList<Room>> theDungeon, final int theDungeonSize){
         Random rand = new Random();
         Room thisRoom = new Room();
@@ -84,8 +113,81 @@ public class Dungeon {
         }
     }
 
+    /**
+     * This method returns a room object that is located at the passed location
+     * @param theY (The Y coordinate)
+     * @param theX (The X coordinate)
+     * @return (Room)
+     */
     Room getContent(final int theY, final int theX) {
         return myDungeon.get(theY).get(theX);
+    }
+    /**
+     * This is a cheat method which reveals all of the items in the dungeon.
+     */
+
+    protected void revealAll() {
+        Room dummyRoom;
+        for(int i = 0; i < myDungeonSize; i++){
+            for (int j = 0; j < myDungeonSize; j++) {
+                dummyRoom = myDungeon.get(i).get(j);
+                dummyRoom.exploreTheRoom();
+            }
+        }
+    }
+
+
+    /**
+     * This method searches for the possible directions and reveals the room accordingly.
+     * @param theLocation (Location of the hero)
+     */
+    protected void visionPotionUser(final Point theLocation) {
+        Room dummyRoom = null;
+        ArrayList<Point> currentLocation = new ArrayList<>();
+        Point dummyPoint = (Point)(theLocation.clone());;
+        boolean north = dummyPoint.y - 1 >= 0;
+        boolean south = dummyPoint.y + 1 <= myDungeonSize-1;
+        boolean west = dummyPoint.x - 1 >= 0;
+        boolean east = dummyPoint.x + 1 <= myDungeonSize-1;
+        if (north) {
+            dummyPoint.translate(0,-1);
+            currentLocation.add(dummyPoint);
+            dummyPoint = (Point)(theLocation.clone());
+        }if (south) {
+            dummyPoint.translate(0,1);
+            currentLocation.add(dummyPoint);
+            dummyPoint = (Point)(theLocation.clone());
+        }if (east) {
+            dummyPoint.translate(1,0);
+            currentLocation.add(dummyPoint);
+            dummyPoint = (Point)(theLocation.clone());
+        }if (west) {
+            dummyPoint.translate(-1,0);
+            currentLocation.add(dummyPoint);
+            dummyPoint = (Point)(theLocation.clone());
+        }
+
+
+        for (Point local: currentLocation){
+            dummyRoom = myDungeon.get(local.y).get(local.x);
+            dummyRoom.exploreTheRoom();
+        }
+    }
+    
+    /**
+     * This method sets the myCheatEnabled to true;
+     */
+    protected void setMyCheatEnabled(){
+        System.out.println("Cheat has been enabled!");
+        myCheatEnabled = true;
+    }
+
+    /**
+     * This method returns the status of the myCheatEnabled field.
+     * @return
+     */
+    protected boolean getMyCheat(){
+        return myCheatEnabled;
     }
     public static void main(String[] args) {
        Dungeon myDungeon= new Dungeon(5);
