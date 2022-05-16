e1f1b6039336be36c25701c615734e8f685355eb
Tinytin06 <52684983+Tinytin06@users.noreply.github.com>
2022-05-16 13:25:51 -0700

    roomtype update 5/16 fixed the legend method so that there is less if else statements

--- src/main/java/Model/RoomType.java
+++ src/main/java/Model/RoomType.java
@@ -31,34 +31,35 @@ public enum RoomType {
     public static String legend() {
         StringBuilder legend = new StringBuilder();
 
-        for (RoomType type : RoomType.values()) {
-            if (type == RoomType.FIGHT) {
-                legend.append("F: Monster, ");
-            } else if (type == RoomType.PIT) {
-                legend.append("P: Pit, ");
-            } else if (type == RoomType.NORMAL) {
-                legend.append("N: Empty Room, ");
-            } else if (type == RoomType.PILLAR) {
-                legend.append("P: Pillar, ");
-            } else if (type == RoomType.EXIT) {
-                legend.append("X: Exit, ");
-            } else if (type == RoomType.ENTRANCE) {
-                legend.append("E: Entrance, ");
-            } else if (type == RoomType.HEALING) {
-                legend.append("H: Healing, ");
-            } else if (type == RoomType.CODING_CROWN_1) {
-                legend.append("K: Coding Crown 1, ");
-            } else if (type == RoomType.CODING_CROWN_2) {
-                legend.append("Q: Coding Crown 2");
-            } else if (type == RoomType.POLYMORPHISM){
-
-            } else if (type == RoomType.ENCAPSULATION){
-
-            } else if (type == RoomType.ABSTRACTION){
-
-            } else if (type == RoomType.INHERITANCE){
-
-            }
+        for (RoomType roomType : RoomType.values()) {
+                legend.append(roomType.type + ": " + roomType +", ");
+//            if (type == RoomType.FIGHT) {
+//                legend.append("F: Monster, ");
+//            } else if (type == RoomType.PIT) {
+//                legend.append("P: Pit, ");
+//            } else if (type == RoomType.NORMAL) {
+//                legend.append("N: Empty Room, ");
+//            } else if (type == RoomType.PILLAR) {
+//                legend.append("P: Pillar, ");
+//            } else if (type == RoomType.EXIT) {
+//                legend.append("X: Exit, ");
+//            } else if (type == RoomType.ENTRANCE) {
+//                legend.append("E: Entrance, ");
+//            } else if (type == RoomType.HEALING) {
+//                legend.append("H: Healing, ");
+//            } else if (type == RoomType.CODING_CROWN_1) {
+//                legend.append("K: Coding Crown 1, ");
+//            } else if (type == RoomType.CODING_CROWN_2) {
+//                legend.append("Q: Coding Crown 2");
+//            } else if (type == RoomType.POLYMORPHISM){
+//
+//            } else if (type == RoomType.ENCAPSULATION){
+//
+//            } else if (type == RoomType.ABSTRACTION){
+//
+//            } else if (type == RoomType.INHERITANCE){
+//
+//            }
         }
         return legend.toString() + ".";
 
